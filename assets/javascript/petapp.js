a896dfcd9a5555c0de9aa3cc83629e725ebc889b
Mark Bui <tvyybui@yahoo.com>
2017-06-15 21:30:40 -0700

    got validation working in form

--- assets/javascript/petapp.js
+++ assets/javascript/petapp.js
@@ -67,18 +67,18 @@ function initMap() {
 
     var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
     var icons = {
-        parking: {
-            name: 'Parking',
-            icon: iconBase + 'parking_lot_maps.png'
-        },
-        library: {
-            name: 'Library',
-            icon: iconBase + 'library_maps.png'
-        },
-        info: {
-            name: 'Info',
-            icon: iconBase + 'info-i_maps.png'
-        }
+      parking: {
+          name: 'Parking',
+          icon: iconBase + 'parking_lot_maps.png'
+      },
+      library: {
+          name: 'Library',
+          icon: iconBase + 'library_maps.png'
+      },
+      info: {
+          name: 'Info',
+          icon: iconBase + 'info-i_maps.png'
+      }
     };
 
     // Store pins you want to display as object with a position and type - where type is the object name of the icon
@@ -115,18 +115,16 @@ function initMap() {
     // for each of the objects in the features list create a marker with the objects position and icon
 
     features.forEach(function(feature) {
-        var newmarker = new google.maps.Marker({
-            position: feature.position,
-            icon: icons[feature.type].icon,
-            map: map
-        });
+    var newmarker = new google.maps.Marker({
+        position: feature.position,
+        icon: icons[feature.type].icon,
+        map: map
+    });
 
         newmarker.addListener('click', function() {
-            console.log("clicked a markr");
-            $("#icon-info").text("Info of this marker");
-            $("#icon-info").css("font-size", "20px");
-
-
+          console.log("clicked a markr");
+          $("#icon-info").text("Info of this marker");
+          $("#icon-info").css("font-size", "20px");
         });
 
     });
@@ -134,21 +132,16 @@ function initMap() {
 
     var legend = document.getElementById("legend");
     for (var key in icons) {
-        var type = icons[key];
-        var name = type.name;
-        var icon = type.icon;
-        var div = document.createElement('div');
-        div.innerHTML = '<img src="' + icon + '"> ' + name;
-        legend.appendChild(div);
+      var type = icons[key];
+      var name = type.name;
+      var icon = type.icon;
+      var div = document.createElement('div');
+      div.innerHTML = '<img src="' + icon + '"> ' + name;
+      legend.appendChild(div);
     }
     map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
 
-
-
-
 }
-
-
 
 
 // THE CODE BELOW IS COMMENTED OUT BECAUSE IT MAKES THE CODE ABOVE MOT WORK
@@ -167,45 +160,9 @@ $(document).ready(function() {
     //   firebase.initializeApp(config);
     //   // setting variables
     //   var database = firebase.database();
-    var yelpID = '';
-    var yelpName = '';
-    var yelpRating = '';
-    var yelpAddress = '';
-    var yelpHours = '';
-    var yelpWebsite = '';
-    var yelpLattitude = '';
-    var yelpLongitude = '';
     var inputSelection = '';
     var inputAddress = '';
     var inputDistance = '';
-
-
-    // function getYelpData() {
-    //   var queryURL = '';
-    //   $('#search-form').on('submit', function(event) {
-    //   event.preventDefault();
-
-    //   var services = document.getElementById('selection-input').value;
-    //   var address = document.getElementById('address-input').value;
-    //   var maxDistance = document.getElementById('distance-input').value;
-    //   $.ajax({
-    //     url: queryURL,
-    //     method: 'GET',
-    //     data: {
-    //       'api-key': 'apikey',
-    //       'services': services
-    //     }
-    //   }).done(function(response){
-    //     var response = response.val();
-    //     yelpID = response.yelpID;
-    //     yelpName = response.yelpName;
-    //     yelpRating = response.yelpRating;
-    //     yelpAddress = response.yelpAddress;
-    //     yelpHours = response.yelpHours;
-    //     yelpWebsite = response.yelpWebsite;
-    //   });
-    //   getGiphy(services);
-    // })
 
     function displayYelpData() {
 
@@ -231,9 +188,22 @@ $(document).ready(function() {
 
     $('#submit-Info').on('click', function(event) {
         event.preventDefault();
-        inputSelection = $('#selection-input').val().trim();
-        inputAddress = $('#pac-input').val().trim();
-        inputDistance = $('#distance-input').val().trim();
+        inputSelection = $('#selection-input').val();
+        if (inputSelection === 'void') {
+          alert('test');
+          return false;
+        }
+        inputAddress = $('#pac-input').val();
+        if (inputAddress === '') {
+          alert('test');
+          return false;
+        };
+        inputDistance = $('#distance-input').val();
+        if (inputDistance === '') {
+          alert('test');
+          return false;
+        };
+
         // console.log('selection: ' + inputSelection);
         // console.log('address: ' + inputAddress);
         // console.log('distance: ' + inputDistance);
